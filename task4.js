41c78f3382aac051794d9bd4edd8b36b27622d6c
Leonid <leo.kutashev@gmail.com>
2025-03-02 09:48:47 +0100

    Create task4.js

---
+++ task4.js
@@ -0,0 +1,5 @@
+const numbers = [2, 4, 6, 8, 10];
+
+const isAllEven = numbers.every(num => num % 2 === 0);
+//Евери проверяет каждый елемент масива на соответсвие условию
+console.log(isAllEven); // Очікуваний результат: true
